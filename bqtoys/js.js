0e88388252d90b9f690cb7dae780b3d19cfe8e98
hadekproject <ing.harolddiazc@gmail.com>
2021-10-28 00:21:27 -0500

    animaciones cards

--- bqtoys/js.js
+++ bqtoys/js.js
@@ -5,6 +5,7 @@ function toggleMenu() {
     navigation.classList.toggle('active');
 }
 
+/*
 let list = document.querySelectorAll('.list');
 let itemBox = document.querySelectorAll('.itemBox');
 
@@ -24,6 +25,31 @@ for (let i = 0; i < list.length; i++) {
             if (itemBox[k].getAttribute('data-item') == dataFilter || dataFilter == "all") {
                 itemBox[k].classList.remove('hide');
                 itemBox[k].classList.add('active');
+            }
+        }
+    })
+}
+*/
+
+let list = document.querySelectorAll('.list');
+let card = document.querySelectorAll('.card');
+
+for (let i = 0; i < list.length; i++) {
+    list[i].addEventListener('click', function () {
+        for (let j = 0; j < list.length; j++) {
+            list[j].classList.remove('active');
+        }
+        this.classList.add('active');
+
+        let dataFilter = this.getAttribute('data-filter');
+
+        for (let k = 0; k < card.length; k++) {
+            card[k].classList.remove('active');
+            card[k].classList.add('hide');
+
+            if (card[k].getAttribute('data-item') == dataFilter || dataFilter == "all") {
+                card[k].classList.remove('hide');
+                card[k].classList.add('active');
             }
         }
     })
